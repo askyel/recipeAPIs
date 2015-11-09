bc07a0ed342e0ef11566313588b6c29db9d6b87d
Ariel Levy <ariel.s.levy1@gmail.com>
2015-11-09 09:52:03 -0500

    navbar

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("master.html")
+	return render_template("master.html")
+
+@app.route("/about")
+def about():
+	return render_template("about.html")
 
 if __name__ == "__main__":
     app.debug = True
