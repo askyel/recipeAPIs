dfc516d069f2133f8e0673b973d2ebd73cb26a9e
JeffreyZou13 <jeffreyzou13@gmail.com>
2015-11-11 16:28:23 -0500

    not much

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 import urllib2, json
 
 def fetchGoogleData(address):
-    key = "AIzaSyDK9pqvEWsu7SWG9b6eZp6KuXKZwM_qQR4"
     url = "http://maps.googleapis.com/maps/geocode/json?adress="+address+"&key="+key
     request = urllib.urlopen(url)
     jsonThing = url.read()
-    latitude = jsonThing["results"]["geometry"]["location"]["lat"]
-    longitude = jsonThing["results"]["geometry"]["location"]["lng"]
+    result = json.loads(jsonThing)
+    if result["status"] == "OK":
+        latitude = result["results"][0]["geometry"]["location"]["lat"]
+        longitude = result["results"][0]["geometry"]["location"]["lng"]
+        place_id = result["results"][0]["geometry"]["place_id"]
