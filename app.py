7e79bca1852e91f6bfe8fff6ca9b66bd94f14d7a
bilingwu <sarawu45@gmail.com>
2015-11-15 11:03:01 -0500

    adding file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import urllib2, json
 
 app = Flask(__name__)
 
---
+++ nyt.py
@@ -0,0 +1,5 @@
+import urllib2
+import jason
+
+def nytArticleSearch():
+    nytKey="7d9f3b88013d99f9f9ab2a8a82545671:19:73424493" #new york times article search api key
